1e8b6fec8e2a97b6c277f679e158370908b237f5
John Philip Magsaysay <magsaysayphil@gmail.com>
2024-10-12 15:26:49 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+kaplay({
+    background: [0, 108, 166],
+});
+
+loadSprite("bean", "assets/bean.png");
+loadSprite("apple", "assets/apple.png");
+
+let speed = 800;
+let borderScreen = width() - 300;
+let score = 0;
+setGravity(1200);
+
+
+const player = add([
+    sprite("bean"),
+    pos(width() / 2, height() - 100),
+    scale(3),
+    anchor("center"),
+    area(),
+    body(),
+    "player",
+
+]);
+
+const platform = add([
+    pos(0, height() - 100),
+    rect(width(), 50),
+    color(0, 30, 46),
+    outline(5),
+    area(),
+    body({isStatic: true}),
+
+]);
+
+
+function spawnApples(){
+    const apples = add([
+        sprite("apple"),
+        pos(rand(100, borderScreen), 30),
+        scale(2),
+        area(),
+        anchor("center"),
+        move(DOWN, 200),
+        "apple",
+    ]);
+
+    wait(rand(1, 2.0), () => {
+        spawnApples();
+    })
+};
+
+spawnApples();
+
+player.onCollide("apple", () => {
+    addKaboom(player.pos);
+})
+
+onKeyDown("left", () => {
+    player.pos.x -= speed * dt();
+});
+
+onKeyDown("right", () => {
+    player.pos.x += speed * dt();
+});
+
